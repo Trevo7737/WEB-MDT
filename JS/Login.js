308e0339e40a45b32fe638e58baa22f18cf43775
trevo7737 <mr.panha7737@gmail.com>
2026-04-11 14:45:24 +0700

    first commit

--- JS/Login.js
+++ JS/Login.js
@@ -9,3 +9,23 @@ openlogin.addEventListener("click", (e) => {
     containerlogin.classList.toggle("active");
   }
 });
+
+// show singup form
+const btnregister = document.querySelector(".singup");
+const displayregister = document.getElementById("form-singup");
+const displaylogin = document.getElementById("form-login");
+const backlogin = document.getElementById("backlogin");
+btnregister.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayregister.style.display = "block";
+  backlogin.style.visibility = "visible";
+  displaylogin.style.display = "none";
+});
+
+// back to login form
+
+backlogin.addEventListener("click", function (e) {
+  displayregister.style.display = "none";
+  backlogin.style.visibility = "hidden";
+  displaylogin.style.display = "block";
+});
--- JS/Register.js
+++
@@ -1,8 +0,0 @@
-const btnregister = document.querySelector(".register");
-
-console.log(btnregister);
-
-btnregister.addEventListener("click", function (e) {
-  document.getElementById("form-register").style.display = "block";
-  document.getElementById("form-login").style.display = "none";
-});
