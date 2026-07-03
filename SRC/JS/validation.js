07972781868e33dbb628fd30af2e5da391ef3d1c
trevo7737 <mr.panha7737@gmail.com>
2026-07-03 11:51:45 +0700

    change some link connect

--- SRC/JS/validation.js
+++ SRC/JS/validation.js
@@ -161,7 +161,7 @@ function chekcinput() {
   setTimeout(() => {
       document.getElementById("login").reset();
     successEl.innerText = "";
-    window.location.href = "../src/dashboard.html";
+    window.location.href = "dashboard.html";
   }, 500);
 }
 
